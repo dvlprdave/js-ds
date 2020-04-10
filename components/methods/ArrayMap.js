87d00a4c383e4f4972935f3f496a6967fb056935
David <devdavee@gmail.com>
2020-04-10 12:18:25 -0400

    added Prism.js for syntax highlighting

--- components/methods/ArrayMap.js
+++ components/methods/ArrayMap.js
@@ -1,6 +1,28 @@
-export default () => (
-    <>
-        <h1>This is the Array Map component</h1>
-        <p>Some description about mapping arrays</p> 
-    </>
-)
+import Prism from 'prismjs'
+import {useEffect} from 'react'
+
+export default () => {
+    useEffect(() => {
+        Prism.highlightAll()
+    }, [])
+    return(
+        <>
+            <h1>This is the Array Map component</h1>
+            <p>Some description about mapping arrays</p> 
+
+            <pre>
+                <code className='language-javascript'>
+                    {`
+                        onSubmit(e) {
+                            e.preventDefault();
+                            const obj = {
+                                title: 'Prism',
+                                company: 'JavaScript'
+                            }
+                        }
+                    `}
+                </code>
+            </pre>
+        </>
+    )
+}
