023bd3f65f009aeb4f95b3c1c031c32d8ee2c220
David <david.quick@macys.com>
2020-04-10 18:54:15 -0400

    prism normalize-whitespace fix

--- components/methods/ArrayMap.js
+++ components/methods/ArrayMap.js
@@ -10,17 +10,17 @@ export default () => {
             <h1>This is the Array Map component</h1>
             <p>Some description about mapping arrays</p> 
 
-            <pre>
-                <code className='language-javascript no-whitespace-normalization'>
-                    {`
-                        onSubmit(e) {
-                            e.preventDefault();
-                            const obj = {
-                                title: 'Prism',
-                                company: 'JavaScript'
-                            }
+            <pre data-line="3">
+                <code className='language-javascript'>
+                {`
+                    onSubmit(e) {
+                        e.preventDefault();
+                        const obj = {
+                            title: 'Prism',
+                            company: 'JavaScript'
                         }
-                    `}
+                    }
+                `}
                 </code>
             </pre>
         </>
